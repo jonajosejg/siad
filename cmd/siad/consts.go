ea61ae7c677d72af6fc53d5aaa4020a4e29b7393
Jonathan Gonzales <rojii0xff@gmail.com>
2024-09-02 00:05:55 -0400

    cmd/build: mainchain rpc port set by default into network addresses

--- cmd/siad/consts.go
+++ cmd/siad/consts.go
@@ -4,9 +4,10 @@
 package main
 
 const (
-	defaultAPIAddr     = "localhost:9980"
-	defaultRPCAddr     = ":9981"
-	defaultRHP2Addr    = ":9982"
-	defaultRHP3TCPAddr = ":9983"
-	defaultRHP3WSAddr  = ":9984"
+	defaultAPIAddr       = "localhost:9980"
+	defaultRPCAddr       = ":9981"
+	defaultRHP2Addr      = ":9982"
+	defaultRHP3TCPAddr   = ":9983"
+	defaultRHP3WSAddr    = ":9984"
+	defaultMainchainAddr = "localhost:8332"
 )
